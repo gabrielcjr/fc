e025e41bfe03a124bc196ca40cd89836875a2ffa
Gabriel Carneiro Jr <gacarneirojr@hotmail.com>
2023-12-12 15:03:28 -0300

    add doctrine fixtures

---
+++ src/FC/UserBundle/DataFixtures/ORM/LoadUser.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace FC\UserBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use FC\UserBundle\Entity\User;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+
+class LoadUser implements FixtureInterface, ContainerAwareInterface {
+
+    private $container;
+
+    public function load(ObjectManager $manager) {
+        $user = new User();
+        $user->setUsername("user")
+            ->setEmail("user@user.com")
+            ->setPassword("password")
+            ->setIsActive(true);
+        
+        $manager->persist($user);
+
+        $admin = new User();
+        $admin->setUsername("admin")
+            ->setEmail("admin@admin.com")
+            ->setPassword("password")
+            ->setIsActive(true);
+        
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+    public function setContainer(ContainerInterface|null $container = null) {
+        $this->container = $container;
+    }
+
+    private function encodePassword($user, $plainPassword) {
+        $encoder = $this->container->get("security.encoder_factory")
+        ->getEncoder($user);
+
+        return $encoder->encodePassword($plainPassword, $user->getSalt());
+    }
+
+}
--- steps2.md
+++ steps2.md
@@ -5,3 +5,19 @@ app/console generate:bundle
 # Doctrine
 
 app/console doctrine:generate:entity
+
+# Create db
+
+app/console doctrine:database:create
+
+app/console doctrine:schema:create
+
+# DataFixtures
+
+Add to composer.json
+
+"doctrine/doctrine-fixtures-bundle": "2.2.*"
+
+composer.phar update
+
+app/console doctrine:fixtures:load
